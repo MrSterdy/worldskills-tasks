5f53154d1b58daae42bde877c9159c8838b8d0e7
MrSterdy <mrsterdy@gmail.com>
2023-11-08 22:24:15 +0300

    Add ability to fully edit tasks

--- static/js/app.js
+++ static/js/app.js
@@ -11,34 +11,84 @@ function refreshTasks() {
     for (const task of tasks) {
         const li = document.createElement("li");
         const header = document.createElement("h2");
-        const deleteBtn = document.createElement("button");
+        const editBtn = document.createElement("button");
 
         header.textContent = task.name;
 
-        deleteBtn.textContent = "X";
-        deleteBtn.addEventListener("click", () => {
-            removeTask(task.name);
-            refreshTasks();
+        editBtn.textContent = "E";
+        editBtn.addEventListener("click", () => {
+            li.replaceChildren();
+
+            const nameInput = document.createElement("input");
+            const subtasksUl = document.createElement("ul");
+            const newSubtaskBtn = document.createElement("button");
+            const deleteBtn = document.createElement("button");
+            const cancelBtn = document.createElement("button");
+            const saveBtn = document.createElement("button");
+
+            nameInput.value = task.name;
+
+            /** @type Subtask[] */
+            const editSubtasks = task.subtasks.map(subtask => ({ ...subtask }));
+
+            const refreshEditSubtasks = () => {
+                subtasksUl.replaceChildren();
+
+                for (const [subtaskI, subtask] of editSubtasks.entries()) {
+                    const subtaskLi = document.createElement("li");
+                    const subtaskInput = document.createElement("input");
+                    const removeSubtaskBtn = document.createElement("button");
+
+                    subtaskInput.value = subtask.name;
+                    subtaskInput.addEventListener("input", () => subtask.name = subtaskInput.value);
+
+                    removeSubtaskBtn.textContent = "X";
+                    removeSubtaskBtn.addEventListener("click", () => {
+                        editSubtasks.splice(subtaskI, 1);
+                        refreshEditSubtasks();
+                    });
+
+                    subtaskLi.append(subtaskInput, removeSubtaskBtn);
+                    subtasksUl.append(subtaskLi);
+                }
+            };
+
+            refreshEditSubtasks();
+
+            newSubtaskBtn.textContent = "N";
+            newSubtaskBtn.addEventListener("click", () => {
+                editSubtasks.push({ name: "Новая подзадача" });
+                refreshEditSubtasks();
+            });
+
+            deleteBtn.textContent = "X";
+            deleteBtn.addEventListener("click", () => {
+                removeTask(task.name);
+                refreshTasks();
+            });
+
+            cancelBtn.textContent = "C";
+            cancelBtn.addEventListener("click", () => refreshTasks());
+
+            saveBtn.textContent = "S";
+            saveBtn.addEventListener("click", () => {
+                removeTask(task.name);
+                createTask({ name: nameInput.value, subtasks: editSubtasks });
+                refreshTasks();
+            });
+
+            li.append(nameInput, subtasksUl, newSubtaskBtn, deleteBtn, cancelBtn, saveBtn);
         });
 
-        li.append(header, deleteBtn);
+        li.append(header, editBtn);
 
         if (task.subtasks.length) {
             const ul = document.createElement("ul");
 
             for (const subtask of task.subtasks) {
                 const subtaskLi = document.createElement("li");
-                const subtaskDeleteBtn = document.createElement("button");
-
                 subtaskLi.textContent = subtask.name;
-
-                subtaskDeleteBtn.textContent = "X";
-                subtaskDeleteBtn.addEventListener("click", () => {
-                    removeSubtaskFrom(task.name, subtask.name);
-                    refreshTasks();
-                });
-
-                ul.append(subtaskLi, subtaskDeleteBtn);
+                ul.append(subtaskLi);
             }
 
             li.append(ul);
@@ -55,7 +105,7 @@ function refreshSubtasks() {
     const subtaskList = document.getElementById("subtasks");
     subtaskList.replaceChildren();
 
-    for (const subtask of subtasks) {
+    for (const [subtaskI, subtask] of subtasks.entries()) {
         const li = document.createElement("li");
         const input = document.createElement("input");
         const deleteBtn = document.createElement("button");
@@ -65,7 +115,7 @@ function refreshSubtasks() {
 
         deleteBtn.textContent = "X";
         deleteBtn.addEventListener("click", () => {
-            subtasks.splice(subtasks.indexOf(subtask), 1);
+            subtasks.splice(subtaskI, 1);
             refreshSubtasks();
         });
 
